ad778f051f3a1e88a916e96a60f52c6c12f3f7ae
Ishraq Hasan <ishraq10199@gmail.com>
2023-05-26 23:17:16 +0600

    fixed a margin issue with feedback in the embed page

--- components/Feedback.js
+++ components/Feedback.js
@@ -5,7 +5,7 @@ import GithubIcon from "./GithubIcon";
 import GoogleIcon from "./GoogleIcon";
 
 const Feedback = ({ author, text, createdAt, provider }) => (
-  <Box borderRadius={4} maxWidth="700px" w="full">
+  <Box borderRadius={4} maxWidth="700px" w="full" mt={4}>
     <Heading size="sm" as="h3" mb={0} color="gray.900" fontWeight="medium">
       {author}
       {provider.includes("github") ? (
@@ -21,7 +21,7 @@ const Feedback = ({ author, text, createdAt, provider }) => (
       {format(parseISO(createdAt), "PPpp")}
     </Text>
     <Text color="gray.800">{text}</Text>
-    <Divider borderColor="gray.200" backgroundColor="gray.200" mt={4} mb={4} />
+    <Divider borderColor="gray.200" backgroundColor="gray.200" mt={4} />
   </Box>
 );
 
